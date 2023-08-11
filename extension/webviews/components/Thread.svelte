1835a970af5c810674d4a75a9421b1bc0b9d1230
taprottoy <taprottoy@mun.ca>
2023-08-11 19:43:56 -0230

    bootstrap design override

--- extension/webviews/components/Thread.svelte
+++ extension/webviews/components/Thread.svelte
@@ -20,13 +20,19 @@
         align-items: center;
         padding: 0.25rem;
     }
-    .card {
+    .thread-card {
         display: flex;
         flex-direction: column;
+        background-color: rgb(46, 48, 48);
+        padding: 0.5rem;
+        gap: 0.25;
+        border-radius: 5px;
+        border: 2px;
+        margin-bottom: 0.5rem;
     }
 </style>
 
-<div class='card'>
+<div class='thread-card'>
     <div class="thread-header">
         <div> {username}</div>
         <OverlayCard handleEdit={handleEditButtonClick} handleDelete={handleDeleteButtonClick}/>
