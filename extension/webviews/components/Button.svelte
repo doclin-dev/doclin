b7c4e919986b2e6d523e823034aad991c076b8a7
taprottoy <taprottoy@mun.ca>
2023-08-26 20:49:54 -0230

    fix button complaint

--- extension/webviews/components/Button.svelte
+++ extension/webviews/components/Button.svelte
@@ -2,23 +2,31 @@
     import Icon from './Icon.svelte';
     export let icon = "";
     export let title = "";
-    export let onClick: () => void;
+    export let onClick: () => void = () => {};
     export let iconWidth = 20;
     export let iconHeight = 20;
     export let size = 'sm';
     export let type = 'submit'; // [text, ]
     export let variant = 'primary'; // [secondary, danger]
 
+    let clicked: boolean;
+
     $: if (!['sm', 'md'].includes(size)) {
         size = 'sm';
     }
 
     $: if (!['text', 'submit'].includes(type)) {
-        type = 'sm';
+        type = 'text';
     }
 
     $: if (!['primary', 'secondary','danger'].includes(variant)) {
         variant = 'primary';
+    }
+
+    function handleKeyPress(event: KeyboardEvent) {
+        if (event.key === 'Enter') {
+            clicked = true;
+        }
     }
 
   </script>
@@ -27,6 +35,7 @@
   <div
     class="button {size} {type} {variant}"
     on:click={onClick}
+    on:keydown={handleKeyPress}
   >
     {#if icon}
       <span class="icon"><Icon width={iconWidth} height={iconHeight} name={icon}/></span>
