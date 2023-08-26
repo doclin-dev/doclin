a45f121cd7091d561ee9c36413b4e6349b3d3d8b
taprottoy <taprottoy@mun.ca>
2023-08-26 20:50:36 -0230

    remove unncessary comment

--- extension/webviews/components/store.js
+++ extension/webviews/components/store.js
@@ -5,5 +5,3 @@ export const editedThreadId = writable(null);
 export const selectedThread = writable(null);
 
 export const editedReplyId = writable(null);
-
-// export const selectedReply = writable(null);
