15253baf4a9cf502e32e1a7f2eadd53bced47a69
Shawon Ibn Kamal <shawon.1165@gmail.com>
2023-09-24 01:59:28 -0230

    move icons mapping to another file

--- extension/webviews/components/Icon.svelte
+++ extension/webviews/components/Icon.svelte
@@ -1,37 +1,12 @@
 <script>
     import Icon from '@iconify/svelte';
-    import replyOutline from '@iconify/icons-mdi/reply-outline';
-    import reloadIcon from '@iconify/icons-mdi/reload';
-    import dotsHorizontal from '@iconify/icons-mdi/dots-horizontal';
-    import keyboardBackspace from '@iconify/icons-mdi/keyboard-backspace';
-    import bellOutline from '@iconify/icons-mdi/bell-outline';
-    import openInNew from '@iconify/icons-mdi/open-in-new';
+    import { iconsMap } from './icons';
 
     export let name;
     export let width = 20;
     export let height = 20;
-    let icon;
-
-    switch(name) {
-        case "reply":
-            icon = replyOutline;
-            break;
-        case "refresh":
-            icon = reloadIcon;
-            break;
-        case "context-menu":
-            icon = dotsHorizontal;
-            break;
-        case "back-icon":
-            icon = keyboardBackspace;
-            break;
-        case "notification":
-            icon = bellOutline;
-            break;
-        case "open-in-new-window":
-            icon = openInNew;
-            break;
-    }
+    
+    const icon = iconsMap[name];
 </script>
 
 <div class="icon">
