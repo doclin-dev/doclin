f50df5097c75619cf7b9892124f3db2c0d237297
taprottoy <taprottoy@mun.ca>
2023-10-02 00:19:41 -0230

    remove unnecessary file

--- extension/webviews/utilities.ts
+++
@@ -1,3 +0,0 @@
-export async function populateThreadMessageField(editor: any, { filePath, threadMessage }: { filePath: string, threadMessage: string }){
-    editor.insertCodeSnippet({filePath, threadMessage});
-};
