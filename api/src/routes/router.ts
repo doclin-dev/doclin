296c05f01b55cdda5b3339a9a7830b0d9ed23e5b
Shawon Ibn Kamal <shawon.1165@gmail.com>
2024-02-10 19:54:45 -0330

    log routes

--- api/src/routes/router.ts
+++ api/src/routes/router.ts
@@ -6,10 +6,19 @@ import { githubCallback } from "../controllers/githubAuthController";
 import organizationRouter from "./organizationRouter";
 import { redeemInvitation } from "../controllers/invitationController";
 import { log } from "../controllers/loggerController";
+import { PRODUCTION } from "../envConstants";
+import logger from "../logger";
 
 const BASE_ORGANIZATION_ROUTE = "/organizations";
 
 const router = express.Router();
+
+if (!PRODUCTION) {
+	router.use((req, _res, next) => {
+		logger.info(`${req.method} ${req.url}`);
+		next();
+	});
+}
 
 router.get("/", (_req, res) => res.send("doclin-api"));
 router.get("/auth/github", passport.authenticate("github", { session: false }));
