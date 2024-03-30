6105af3a13284e4404be6b68d65aa7ca63362afe
Shawon Ibn Kamal <shawon.1165@gmail.com>
2024-03-30 01:30:33 -0230

    unit test and cleanup

--- extension/src/utils/waitForSidebarToShow.ts
+++ extension/src/utils/waitForSidebarToShow.ts
@@ -2,28 +2,43 @@ import * as vscode from 'vscode';
 import { SidebarLoadingStatus } from '../enums';
 
 const GET_SIDEBAR_LOADING_STATUS = 'getSidebarLoadingStatus';
+const INTERVAL = 500;
+const TIMEOUT = 5000;
 let sidebarLoadingStatus: SidebarLoadingStatus = SidebarLoadingStatus.UNMOUNTED;
 
 export const waitForSidebarStatus = (webview: vscode.Webview, targetLoadingStatus: SidebarLoadingStatus) => {
 	updateSidebarReadyStatus(webview);
 
-	return new Promise<void>((resolve, reject) => {
-		let timeout: NodeJS.Timeout;
-		const interval = setInterval(() => {
-			if (sidebarLoadingStatus >= targetLoadingStatus) {
-				clearInterval(interval);
-				clearTimeout(timeout);
-				resolve();
-			} else {
-				updateSidebarReadyStatus(webview);
-			}
-		}, 500);
+	let timeout: NodeJS.Timeout;
+	let interval: NodeJS.Timeout;
 
-		timeout = setTimeout(() => {
-			clearInterval(interval);
-			reject(new Error("Please open doclin sidebar to add a comment"));
-		}, 5000);
+	const intervalTimer = new Promise<void>((resolve, reject) => {
+		interval = startInterval(targetLoadingStatus, webview, resolve);
+		timeout = startTimeout(reject);
 	});
+
+	intervalTimer.finally(() => {
+		clearInterval(interval);
+		clearTimeout(timeout);
+	});
+
+	return intervalTimer;
+};
+
+const startInterval = (targetLoadingStatus: SidebarLoadingStatus, webview: vscode.Webview, resolve: () => void) => {
+	return setInterval(() => {
+		if (sidebarLoadingStatus >= targetLoadingStatus) {
+			resolve();
+		} else {
+			updateSidebarReadyStatus(webview);
+		}
+	}, INTERVAL);
+};
+
+const startTimeout = (reject: (reason?: any) => void) => {
+	return setTimeout(() => {
+		reject(new Error("Please open doclin sidebar to add a comment"));
+	}, TIMEOUT);
 };
 
 const updateSidebarReadyStatus = (webview: vscode.Webview) => {
