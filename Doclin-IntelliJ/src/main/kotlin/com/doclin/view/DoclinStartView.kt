5dc4027ba9fa32546552147da95ff9c93f6bfa2a
Leyon <leyonibnkamal@gmail.com>
2025-03-15 03:39:10 -0400

    added a base java swing scrollable ui to contain other interface components

--- Doclin-IntelliJ/src/main/kotlin/com/doclin/view/DoclinStartView.kt
+++ Doclin-IntelliJ/src/main/kotlin/com/doclin/view/DoclinStartView.kt
@@ -1,27 +1,19 @@
 package com.doclin.view
 
-import javax.swing.*
 import java.awt.BorderLayout
+import javax.swing.*
 
 class DoclinStartView : JPanel() {
 
     init {
-        layout = BorderLayout()
-
-        val titleLabel = JLabel("Doclin Start")
-        titleLabel.horizontalAlignment = SwingConstants.CENTER
-        add(titleLabel, BorderLayout.NORTH)
-
-        // Add any other components you need for the start view
         val contentPanel = JPanel()
         contentPanel.layout = BoxLayout(contentPanel, BoxLayout.Y_AXIS)
 
-        val welcomeLabel = JLabel("Welcome to Doclin!")
-        contentPanel.add(welcomeLabel)
+        val scrollPane = JScrollPane(contentPanel)
+        scrollPane.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED
+        scrollPane.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
 
-        add(contentPanel, BorderLayout.CENTER)
-
-        val someButton = JButton("Do Something")
-        add(someButton, BorderLayout.SOUTH)
+        layout = BorderLayout()
+        add(scrollPane, BorderLayout.CENTER)
     }
 }
